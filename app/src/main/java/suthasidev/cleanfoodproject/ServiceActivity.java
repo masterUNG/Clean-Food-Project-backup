5a3c59ee3561f27510b9d12fb333376d5f3a9822
MasterUNG <phrombutr@gmail.com>
2017-10-06 13:07:50 +0700

    Menu OK

--- app/src/main/java/suthasidev/cleanfoodproject/ServiceActivity.java
+++ app/src/main/java/suthasidev/cleanfoodproject/ServiceActivity.java
@@ -5,6 +5,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -21,6 +25,9 @@ public class ServiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentFragment, new AddRecipeFragment())
@@ -30,5 +37,33 @@ public class ServiceActivity extends AppCompatActivity {
 
     }   //Main Method
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+//        Recipe
+        if (item.getItemId() == R.id.itemAddRecipe) {
+            Log.d("6octV1", "Click Add Recipe");
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contentFragment, new AddRecipeFragment())
+                    .commit();
+        }
+
+
+//        Restaurant
+        if (item.getItemId() == R.id.itemAddRestaurant) {
+            Log.d("6octV1", "Click Add Restaurant");
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contentFragment, new AddRestaurantFragment())
+                    .commit();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }   //Main Class
