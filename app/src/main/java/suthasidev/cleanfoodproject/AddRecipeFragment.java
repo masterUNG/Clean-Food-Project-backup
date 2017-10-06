5b18da89b327236cb5196d62f64318c5967dc07c
MasterUNG <phrombutr@gmail.com>
2017-10-06 11:56:56 +0700

    before Lunch

---
+++ app/src/main/java/suthasidev/cleanfoodproject/AddRecipeFragment.java
@@ -0,0 +1,22 @@
+package suthasidev.cleanfoodproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by masterung on 6/10/2017 AD.
+ */
+
+public class AddRecipeFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_recipe, container, false);
+        return view;
+    }
+}
--- app/src/main/java/suthasidev/cleanfoodproject/ServiceActivity.java
+++ app/src/main/java/suthasidev/cleanfoodproject/ServiceActivity.java
@@ -12,10 +12,7 @@ import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
 
-    //Explicit
-    private TextView showUserTextView;
-    private ListView recipeListView;
-    private String nameUserString;
+
 
 
     @Override
@@ -24,75 +21,14 @@ public class ServiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
 
-        //Bind Widget
-        bindWidget();
-
-        //Show Name
-        showName();
-
-        //Create ListView
-        createListView();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragment, new AddRecipeFragment())
+                    .commit();
+        }
 
 
     }   //Main Method
 
-    private void createListView() {
-        //Read All SQLite
-        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
-                MODE_PRIVATE, null);
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + MyManage.table_recipe, null);
-        cursor.moveToFirst();
 
-        int intCount = cursor.getCount();
-        final String[] recipeString = new String[intCount];
-        final String[] ingredientsStrings = new String[intCount];
-        final String[] howToStrings = new String[intCount];
-        final String[] descriptionStrings = new String[intCount];
-        final String[] imageRecipeStrings = new String[intCount];
-
-        for (int i = 0; i < intCount; i++) {
-            recipeString[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Recipe));
-            ingredientsStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Ingredients));
-            howToStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_HowTo));
-            descriptionStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Description));
-            imageRecipeStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_ImageRecipe));
-
-            cursor.moveToNext();
-        }   //for
-        cursor.close();
-
-        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, imageRecipeStrings, recipeString);
-        recipeListView.setAdapter(myAdapter);
-
-        recipeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-
-                Intent intent = new Intent(ServiceActivity.this, RecipeDetailActivity.class);
-                intent.putExtra("nameRecipe", recipeString[i]);
-                intent.putExtra("urlRecipe", imageRecipeStrings[i]);
-                intent.putExtra("Ingredients", ingredientsStrings[i]);
-                intent.putExtra("HowTo", howToStrings[i]);
-                intent.putExtra("Descrip", descriptionStrings[i]);
-                intent.putExtra("nameUser", nameUserString);
-                startActivity(intent);
-
-            }   //OnItemClick
-        });
-
-    }   //createListView
-
-    private void showName() {
-
-        //Receive Value From Intent
-        nameUserString = getIntent().getStringExtra("userName");
-        showUserTextView.setText("Welcome" + nameUserString);
-
-    }   //showName
-
-    private void bindWidget() {
-        showUserTextView = (TextView) findViewById(R.id.list_txt);
-        recipeListView = (ListView) findViewById(R.id.listView);
-
-    }
 }   //Main Class
