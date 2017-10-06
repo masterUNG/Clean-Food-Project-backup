2b0b96426abf9a5440a6e2dbb3a22d3b6c953936
MasterUNG <phrombutr@gmail.com>
2017-10-06 14:07:25 +0700

    Show Image Success

--- app/src/main/java/suthasidev/cleanfoodproject/AddRecipeFragment.java
+++ app/src/main/java/suthasidev/cleanfoodproject/AddRecipeFragment.java
@@ -1,11 +1,17 @@
 package suthasidev.cleanfoodproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by masterung on 6/10/2017 AD.
@@ -13,10 +19,64 @@ import android.view.ViewGroup;
 
 public class AddRecipeFragment extends Fragment{
 
+    private Uri uri;
+    private ImageView imageView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_add_recipe, container, false);
         return view;
     }
-}
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Image Controller
+        imageController();
+
+
+    }
+
+    private void imageController() {
+        imageView = (ImageView) getView().findViewById(R.id.imvMyImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String tag = "6octV2";
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            //Success Choose Image
+            Log.d(tag, "Choose Success");
+            try {
+
+                uri = data.getData();
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity()
+                        .getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+
+        }
+    }
+}   // Main Class
